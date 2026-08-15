239ffb8fa07b0d45d31b0cb9059740174129d889
Frost Edson <edsonchan6@gmail.com>
2026-08-15 21:14:31 +0300

    Began porting of utilfuns to rust from cpp to rust. This is to help with crashes and not renderring of corrupted modules like NET from bible.org

--- src/utilfuns-rs/target/debug/deps/sword_utilfuns-a0cfa9525e82b5c1.d
+++
@@ -1,5 +0,0 @@
-/Users/zoebrooklyn/Documents/Dev/sword/src/utilfuns-rs/target/debug/deps/sword_utilfuns-a0cfa9525e82b5c1.d: src/lib.rs
-
-/Users/zoebrooklyn/Documents/Dev/sword/src/utilfuns-rs/target/debug/deps/sword_utilfuns-a0cfa9525e82b5c1: src/lib.rs
-
-src/lib.rs:
--- src/utilfuns-rs/target/debug/deps/sword_utilfuns-fc15599329a67119.d
+++
@@ -1,5 +0,0 @@
-/Users/zoebrooklyn/Documents/Dev/sword/src/utilfuns-rs/target/debug/deps/sword_utilfuns-fc15599329a67119.d: src/lib.rs
-
-/Users/zoebrooklyn/Documents/Dev/sword/src/utilfuns-rs/target/debug/deps/sword_utilfuns-fc15599329a67119: src/lib.rs
-
-src/lib.rs:
